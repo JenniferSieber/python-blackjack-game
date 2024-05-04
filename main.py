510ee4fea2f0211e98eb448d7705ad149f9b61a7
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2024-05-04 09:28:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -103,21 +103,17 @@ class Game:
         deck.shuffle()
         player_hand = Hand()
         dealer_hand = Hand(dealer=True)
-  
         for i in range(2):
           player_hand.add_card(deck.deal(1))
           dealer_hand.add_card(deck.deal(1))
-        
         print()
         print('*' * 30) # divider
         print(f'Game {game_number} of {games_to_play}')
         print('*' * 30)
         player_hand.display()
         dealer_hand.display()
-  
         if self.check_winner(player_hand, dealer_hand):
           continue 
-  
         choice = ''
         while player_hand.get_value() < 21 and choice not in ['s', 'stand']:
           choice = input("Please choose 'Hit' or 'Stand': ").lower()
@@ -128,27 +124,20 @@ class Game:
           if choice in ['hit', 'h']:
             player_hand.add_card(deck.deal(1))
             player_hand.display()
-  
         if self.check_winner(player_hand, dealer_hand):
           continue
-  
         player_hand_value = player_hand.get_value()
         dealer_hand_value = dealer_hand.get_value()
         while dealer_hand_value < 17:
           dealer_hand.add_card(deck.deal(1))
           dealer_hand_value = dealer_hand.get_value()
-  
         dealer_hand.display(show_all_dealer_cards=True)
-  
         if self.check_winner(player_hand, dealer_hand):
           continue
-  
         print("Final Results")
         print("Your hand:", player_hand_value)
         print("Dealer's hand:", dealer_hand_value)
-  
         self.check_winner(player_hand, dealer_hand, True)
-  
       print("\nThanks for playing!")
 
   def check_winner(self, player_hand, dealer_hand, game_over=False):
